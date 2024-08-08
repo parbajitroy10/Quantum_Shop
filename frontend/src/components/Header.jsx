3dda9e489164360ea342676c6be92cbc4add1a42
parbajitroy10 <parbajitroy@gmail.com>
2024-08-09 02:41:12 +0600

    cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import { Navbar, Nav, Container, NavLink } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavLink } from 'react-bootstrap';
 import{ FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png'
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -17,13 +20,18 @@ const Header = () => {
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
                         <LinkContainer to='/cart'>
-                        <Nav.Link>
-                            <FaShoppingCart/> Cart
+                            <Nav.Link>
+                                <FaShoppingCart/> Cart
+                                {cartItems.length > 0 && (
+                                    <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                        { cartItems.reduce((a,c ) => a + c.qty, 0) }
+                                    </Badge>
+                                )}
                             </Nav.Link>
                         </LinkContainer>        
                         <LinkContainer to='/login'>
-                        <Nav.Link href='/login'>
-                            <FaUser/> Sing In
+                            <Nav.Link href='/login'>
+                                <FaUser/> Sing In
                             </Nav.Link>
                         </LinkContainer>
                     </Nav>
--- frontend/src/screens/Productscreen.jsx
+++ frontend/src/screens/Productscreen.jsx
@@ -1,31 +1,41 @@
-import { useState, useEffect } from 'react';
-import{ useParams } from "react-router-dom";
+import { useState } from "react";
+import{ useParams, useNavigate } from "react-router-dom";
 import{ Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { useDispatch } from "react-redux";
 import Rating from '../components/Rating'
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import {addToCart} from '../slices/cartSlice';
 
 const Productscreen = () => {
-    const [product, setProduct] = useState({});
-
     const { id: productId } = useParams();
-
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const{ data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }  
-
-        fetchProduct();
-    }, [productId]); 
     
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [qty, setQty] = useState(1);
+
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+
+    const addToCartHandler = () => {
+        dispatch(addToCart({ ...product, qty }));
+        navigate('/cart');
+    };
 
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
             Go Back
             </Link>
-            <Row>
+
+            { isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{ error?.data?.message || error.error }</Message>
+            ) : (
+                <Row>
                 <Col md={5}>
                     <Image src={product.image} alt={product.name} fluid/>
                 </Col>
@@ -61,12 +71,30 @@ const Productscreen = () => {
                                         <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
                                     </Col>
                                 </Row>
-                            </ListGroup.Item>  
+                            </ListGroup.Item>
+
+                                {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <row>
+                                            <Col>Qty</Col>
+                                            <Col>
+                                                <Form.Control as='select' value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+                                                    {[...Array(product.countInStock).keys()].map((x) => (
+                                                        <option key={x + 1} value={x + 1}>
+                                                            {x + 1}
+                                                        </option>
+                                                    ))}
+                                                </Form.Control>
+                                            </Col>
+                                        </row>
+                                    </ListGroup.Item>
+                                )}  
                             <ListGroup.Item>
                                 <Button
                                     className='btn-block'
                                     type='button'
                                     disabled={product.countInStock === 0}
+                                    onClick={addToCartHandler}
                                 >
                                     Add to Cart    
                                 </Button>    
@@ -74,7 +102,7 @@ const Productscreen = () => {
                         </ListGroup>
                     </Card>
                 </Col>
-            </Row>
+            </Row>) }
         </>
     );
 };
