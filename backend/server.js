6c3efbb251402f5b0eacfb47acae9c762c852c14
parbajitroy10 <parbajitroy@gmail.com>
2024-08-13 01:32:28 +0600

    chup*

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notfound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT;// || 5000;
 
 connectDB(); // Connect to MongoDB
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API is running...');
@@ -16,6 +25,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notfound);
 app.use(errorHandler);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import{
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import{ createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux'; 
 import store from './store';
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,12 +10,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/Productscreen';
+import CartScreen from './screens/CartScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true} path="/" element={<HomeScreen/>} />
       <Route path="/product/:id" element={<ProductScreen/>} />
+      <Route path="/cart" element={<CartScreen/>} />
     </Route>
   )
 )
