d5af0133a92ad4f4fe5c77ffba83f0b9439a7549
parbajitroy10 <parbajitroy@gmail.com>
2024-08-15 00:35:58 +0600

    hehe

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
-import FormContainer from "../components/FormContainer"
+import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('')
@@ -10,11 +10,31 @@ const LoginScreen = () => {
     const submitHandler = (e) => {
         e.preventDefault()
         console.log('submit')
-    }
+    };
 
   return (
-    <div>LoginScreen</div>
-  )
-}
+    <FormContainer>
+      <h1>Sign In</h1>
 
-export default LoginScreen
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email' className='my-3'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>  
+        </Form.Group>  
+
+        <Form.Group controlId='password' className='my-3'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>  
+        </Form.Group> 
+
+        <Button type="submit" variant='primary' className='mt-3'>Sign In</Button>
+      </Form> 
+
+      <Row className='py-3'>
+        <Col>New Customer? <Link to='/register'>Register</Link></Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
